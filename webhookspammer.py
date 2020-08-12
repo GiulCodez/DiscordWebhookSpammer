97975029a9caa0a5954f0719e34862b9fdcad5e4
GiulCodez <60546384+GiulCodez@users.noreply.github.com>
2020-08-12 19:45:59 +0000

    Added Multiple Things
    Added MultiThreading & added some colors and ascii to look it better

--- webhookspammer.py
+++ webhookspammer.py
@@ -1,16 +1,48 @@
 import requests
+from colorama import Fore, Back, Style
+import os
+import platform
+import threading
 
-webhook = input(">>Please enter the Webhook: ") #input for webhook url
-print("Weebhook " + webhook + " set") #debug point
+print(Fore.CYAN + """
+
+
+
+ █     █░▓█████  ▄▄▄▄    ██░ ██  ▒█████   ▒█████   ██ ▄█▀  ██████  ██▓███   ▄▄▄       ███▄ ▄███▓ ███▄ ▄███▓▓█████  ██▀███  
+▓█░ █ ░█░▓█   ▀ ▓█████▄ ▓██░ ██▒▒██▒  ██▒▒██▒  ██▒ ██▄█▒ ▒██    ▒ ▓██░  ██▒▒████▄    ▓██▒▀█▀ ██▒▓██▒▀█▀ ██▒▓█   ▀ ▓██ ▒ ██▒
+▒█░ █ ░█ ▒███   ▒██▒ ▄██▒██▀▀██░▒██░  ██▒▒██░  ██▒▓███▄░ ░ ▓██▄   ▓██░ ██▓▒▒██  ▀█▄  ▓██    ▓██░▓██    ▓██░▒███   ▓██ ░▄█ ▒
+░█░ █ ░█ ▒▓█  ▄ ▒██░█▀  ░▓█ ░██ ▒██   ██░▒██   ██░▓██ █▄   ▒   ██▒▒██▄█▓▒ ▒░██▄▄▄▄██ ▒██    ▒██ ▒██    ▒██ ▒▓█  ▄ ▒██▀▀█▄  
+░░██▒██▓ ░▒████▒░▓█  ▀█▓░▓█▒░██▓░ ████▓▒░░ ████▓▒░▒██▒ █▄▒██████▒▒▒██▒ ░  ░ ▓█   ▓██▒▒██▒   ░██▒▒██▒   ░██▒░▒████▒░██▓ ▒██▒
+░ ▓░▒ ▒  ░░ ▒░ ░░▒▓███▀▒ ▒ ░░▒░▒░ ▒░▒░▒░ ░ ▒░▒░▒░ ▒ ▒▒ ▓▒▒ ▒▓▒ ▒ ░▒▓▒░ ░  ░ ▒▒   ▓▒█░░ ▒░   ░  ░░ ▒░   ░  ░░░ ▒░ ░░ ▒▓ ░▒▓░
+  ▒ ░ ░   ░ ░  ░▒░▒   ░  ▒ ░▒░ ░  ░ ▒ ▒░   ░ ▒ ▒░ ░ ░▒ ▒░░ ░▒  ░ ░░▒ ░       ▒   ▒▒ ░░  ░      ░░  ░      ░ ░ ░  ░  ░▒ ░ ▒░
+  ░   ░     ░    ░    ░  ░  ░░ ░░ ░ ░ ▒  ░ ░ ░ ▒  ░ ░░ ░ ░  ░  ░  ░░         ░   ▒   ░      ░   ░      ░      ░     ░░   ░ 
+    ░       ░  ░ ░       ░  ░  ░    ░ ░      ░ ░  ░  ░         ░                 ░  ░       ░          ░      ░  ░   ░     
+                      ░                                                                                                                                                                          
+"""
++ Fore.YELLOW + "https://github.com/GiulCodez/DiscordWebhookSpammer")
+print("")
+
+webhook = input(Fore.RED +">>Please enter the Webhook: ") #input for webhook url
 text = input(">>Please enter the Message that should be spammed: ") #asks for message
-print("Message " + text + " set") #debug point
 
+if platform.system() == "Windows": #checking the OS of the Devise Running the tool
+    clearcmd = "cls"
+else:
+    clearcmd = "clear"
+
+os.system(clearcmd)
 
 data = {
     "content": text #data for webhook as json
 }
 
+def a():
+            requests.post(webhook, data=data) #sends data to webhook
+            print(Fore.MAGENTA + "Sent Message " + text + " successful") #message for feedback lol
+            return
+
+
 while True: #loop
     if True:
-        requests.post(webhook, data=data) #sends data to webhook
-        print("Sent Message " + text + " successful") #message for feedback lol
+        tr = threading.Thread(target=a)
+        tr.start()
