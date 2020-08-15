d87d31bccaeab264fcfebd6060561004db9ce02a
swarfey <45124381+swarfey@users.noreply.github.com>
2020-08-15 12:43:53 +0200

    Add files via upload

--- webhookspammer.py
+++ webhookspammer.py
@@ -1,8 +1,5 @@
-import requests
+import requests, os, platform, time
 from colorama import Fore, Back, Style
-import os
-import platform
-import threading
 
 print(Fore.CYAN + """
 
@@ -22,10 +19,10 @@ print(Fore.CYAN + """
 + Fore.YELLOW + "https://github.com/GiulCodez/DiscordWebhookSpammer")
 print("")
 
-webhook = input(Fore.RED +">>Please enter the Webhook: ") #input for webhook url
-text = input(">>Please enter the Message that should be spammed: ") #asks for message
+webhook = input(Fore.RED +">>Please enter the Webhook: ") # input for webhook url
+text = input(">>Please enter the Message that should be spammed: ") # asks for message
 
-if platform.system() == "Windows": #checking the OS of the Devise Running the tool
+if platform.system() == "Windows": # checking the OS of the device running the tool
     clearcmd = "cls"
 else:
     clearcmd = "clear"
@@ -33,16 +30,23 @@ else:
 os.system(clearcmd)
 
 data = {
-    "content": text #data for webhook as json
+    "content": text # data for webhook as json
 }
-
-def a():
-            requests.post(webhook, data=data) #sends data to webhook
-            print(Fore.MAGENTA + "Sent Message " + text + " successful") #message for feedback lol
-            return
-
-
+def send(i):
+    res = requests.post(webhook, data=data) # sends data to webhook
+    try:
+        print(Fore.RED + 'getting ratelimited, waiting ' + str(res.json()["retry_after"]) + 'ms.')
+        # response: {'global': False, 'message': 'You are being rate limited.', 'retry_after': 12345)}
+        time.sleep(res.json()["retry_after"]/1000)
+        res = 'waited ' + Fore.RED + str(res.json()["retry_after"]) + 'ms.'
+    except:
+        i += 1
+        res = "Sent message " + text + " successful."
+    print(Fore.MAGENTA + res + Fore.MAGENTA + ' Amount of messages already sent: ' + Fore.CYAN + str(i)) # message for feedback lol
+    return i
+i = 0
 while True: #loop
-    if True:
-        tr = threading.Thread(target=a)
-        tr.start()
+   i = send(i) # since its getting ratelimited anyways, there should be no need for threading
+   # if True:
+   #     tr = threading.Thread(target=send)
+   #     tr.start()
